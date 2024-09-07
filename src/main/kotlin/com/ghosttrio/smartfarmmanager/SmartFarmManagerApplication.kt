9f7ce673d42b1a861c03d90f45fbd7805badcef3
Ghosttrio <ghosttrio@naver.com>
2024-09-07 21:05:55 +0900

    first commit

---
+++ src/main/kotlin/com/ghosttrio/smartfarmmanager/SmartFarmManagerApplication.kt
@@ -0,0 +1,11 @@
+package com.ghosttrio.smartfarmmanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SmartFarmManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<SmartFarmManagerApplication>(*args)
+}
---
+++ src/test/kotlin/com/ghosttrio/smartfarmmanager/SmartFarmManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ghosttrio.smartfarmmanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SmartFarmManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
